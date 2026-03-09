7182fac703d487cef613cb4de8e0a60f1d5ab4e0
Artem Shunda <Skorpion60408@gmail.com>
2026-03-09 20:51:03 +0100

    Refactor layout: add templates and kanban UI
    Move head.php into templates and add reusable templates (nav.php, dots.php, plus.php). Update index.php to require templates/head.php and templates/nav.php, replace inline nav with includes, adjust SVG size, and add kanban markup (To Do / In Progress / Done) with card counter and plus buttons. Update css/main.css to use responsive heights (95vh), adjust grid rows, remove redundant html/body reset, and add styles for the kanban layout and elements.

---
+++ templates/nav.php
@@ -0,0 +1,84 @@
+<nav>
+            <div class="nav-element active">
+                <svg viewBox="-6 -6 36 36" fill="none" xmlns="http://www.w3.org/2000/svg" stroke="#000">
+                    <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                    <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+                    <g id="SVGRepo_iconCarrier">
+                        <path
+                            d="M10.5 19.9V4.1C10.5 2.6 9.86 2 8.27 2H4.23C2.64 2 2 2.6 2 4.1V19.9C2 21.4 2.64 22 4.23 22H8.27C9.86 22 10.5 21.4 10.5 19.9Z"
+                            stroke="#ffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
+                        <path
+                            d="M22 12.9V4.1C22 2.6 21.36 2 19.77 2H15.73C14.14 2 13.5 2.6 13.5 4.1V12.9C13.5 14.4 14.14 15 15.73 15H19.77C21.36 15 22 14.4 22 12.9Z"
+                            stroke="#ffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
+                    </g>
+                </svg>
+            </div>
+            <div class="nav-element">
+                <svg viewBox="-6 -6 36 36" xmlns="http://www.w3.org/2000/svg" fill="none">
+                    <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                    <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+                    <g id="SVGRepo_iconCarrier">
+                        <path stroke="#ffffff" stroke-linejoin="round" stroke-width="2"
+                            d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"></path>
+                        <path stroke="#ffffff" stroke-linejoin="round" stroke-width="2"
+                            d="M10.47 4.32c.602-1.306 2.458-1.306 3.06 0l.218.473a1.684 1.684 0 0 0 2.112.875l.49-.18c1.348-.498 2.66.814 2.162 2.163l-.18.489a1.684 1.684 0 0 0 .875 2.112l.474.218c1.305.602 1.305 2.458 0 3.06l-.474.218a1.684 1.684 0 0 0-.875 2.112l.18.49c.498 1.348-.814 2.66-2.163 2.162l-.489-.18a1.684 1.684 0 0 0-2.112.875l-.218.473c-.602 1.306-2.458 1.306-3.06 0l-.218-.473a1.684 1.684 0 0 0-2.112-.875l-.49.18c-1.348.498-2.66-.814-2.163-2.163l.181-.489a1.684 1.684 0 0 0-.875-2.112l-.474-.218c-1.305-.602-1.305-2.458 0-3.06l.474-.218a1.684 1.684 0 0 0 .875-2.112l-.18-.49c-.498-1.348.814-2.66 2.163-2.163l.489.181a1.684 1.684 0 0 0 2.112-.875l.218-.474Z">
+                        </path>
+                    </g>
+                </svg>
+            </div>
+            <div class="nav-element">
+                <svg viewBox="-7 -6 38 38" fill="none" xmlns="http://www.w3.org/2000/svg" stroke="#ffffff">
+                    <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                    <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+                    <g id="SVGRepo_iconCarrier">
+                        <g clip-path="url(#ffffffclip0_429_11023)">
+                            <path
+                                d="M6 19V10C6 6.68629 8.68629 4 12 4V4C15.3137 4 18 6.68629 18 10V19M6 19H18M6 19H4M18 19H20"
+                                stroke="#ffffff" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round">
+                            </path>
+                            <path d="M11 22L13 22" stroke="#ffffff" stroke-width="2.5" stroke-linecap="round"
+                                stroke-linejoin="round"></path>
+                            <circle cx="12" cy="3" r="1" stroke="#ffffff" stroke-width="2.5"></circle>
+                        </g>
+                        <defs>
+                            <clipPath id="clip0_429_11023">
+                                <rect width="24" height="24" fill="white"></rect>
+                            </clipPath>
+                        </defs>
+                    </g>
+                </svg>
+            </div>
+            <div class="nav-element">
+                <svg viewBox="-8 -7.5 64 64" xmlns="http://www.w3.org/2000/svg" fill="#ffffff" stroke="#ffffff">
+                    <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                    <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+                    <g id="SVGRepo_iconCarrier">
+                        <title>information-circle</title>
+                        <g id="Layer_2" data-name="Layer 2">
+                            <g id="icons_Q2" data-name="icons Q2">
+                                <path
+                                    d="M24,2A22,22,0,1,0,46,24,21.9,21.9,0,0,0,24,2Zm0,40A18,18,0,1,1,42,24,18.1,18.1,0,0,1,24,42Z">
+                                </path>
+                                <path d="M24,20a2,2,0,0,0-2,2V34a2,2,0,0,0,4,0V22A2,2,0,0,0,24,20Z"></path>
+                                <circle cx="24" cy="14" r="2"></circle>
+                            </g>
+                        </g>
+                    </g>
+                </svg>
+            </div>
+            <div class="nav-element">
+                <svg viewBox="-6 -5 36 36" fill="none" xmlns="http://www.w3.org/2000/svg" stroke="#fffffff">
+                    <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                    <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+                    <g id="SVGRepo_iconCarrier">
+                        <path
+                            d="M3.32031 11.6835C3.32031 16.6541 7.34975 20.6835 12.3203 20.6835C16.1075 20.6835 19.3483 18.3443 20.6768 15.032C19.6402 15.4486 18.5059 15.6834 17.3203 15.6834C12.3497 15.6834 8.32031 11.654 8.32031 6.68342C8.32031 5.50338 8.55165 4.36259 8.96453 3.32996C5.65605 4.66028 3.32031 7.89912 3.32031 11.6835Z"
+                            stroke="#ffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
+                    </g>
+                </svg>
+            </div>
+            <p></p>
+            <div class="nav-element pp">
+                <img class="profile-picture" src="img/user-photo.jpg" alt="">
+            </div>
+        </nav>
